bdbdfbb798559d55bc56b43bb9f2370ca2509758
Paulo Henrique Freitas <paulo-henriquee@hotmail.com>
2016-08-17 01:40:10 -0300

    First Commit

---
+++ LojaVitual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LojaVitual.Web.MvcApplication" Language="C#" %>
---
+++ LojaVitual.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LojaVitual.Web.Startup))]
+namespace LojaVitual.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
